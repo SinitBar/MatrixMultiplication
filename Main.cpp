fd3032f1aee98caa2e47237de622887e381d2410
SinitBar <varka.sinitsckaya@yandex.ru>
2020-12-05 00:19:56 +0300

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -5,11 +5,12 @@ int main()
 	ofstream out("out.txt");
 	int size1i, size1j, size2i, size2j;
 	in >> size1i >> size1j >> size2i >> size2j;
-	int n2_matrix_degree = get_future_n2_matrix_degree(size1i, size1j, size2j);
-	Square_matrix first_matrix(size1i, size1j, n2_matrix_degree, in);
-	Square_matrix second_matrix(size2i, size2j, n2_matrix_degree, in);
+	int n2_matrix_degree = get_future_square_matrix_degree(size1i, size1j, size2j);
+	Square_matrix first_matrix, second_matrix;
+	first_matrix.stream_fill_square_matrix(size1i, size1j, n2_matrix_degree, in);
+	second_matrix.stream_fill_square_matrix(size2i, size2j, n2_matrix_degree, in);
 	Square_matrix result(n2_matrix_degree);
-	result.multimatrix(first_matrix, second_matrix);
+	result.multi_square_matrix(first_matrix, second_matrix);
 	for (int i = 0; i < size1i; i++)
 	{
 		for (int j = 0; j < size2j; j++)
@@ -26,7 +27,8 @@ int main()
 	in.close();
 	out.close();
 	in.open("right_out.txt");
-	Square_matrix right_result(size1i, size2j, n2_matrix_degree, in);
+	Square_matrix right_result;
+	right_result.stream_fill_square_matrix(size1i, size2j, n2_matrix_degree, in);
 	if (result == right_result)
 		cout << "you won!!!" << endl;
 	return 0;
